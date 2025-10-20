f9d16bf86d56e5843ef6089774ffe0ad6f610ec6
Nicole Lins Coelho <nicolelinscoelho@gmail.com>
2025-10-20 14:32:44 -0300

    Página de manutenção adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { Home } from "./Pages/Home/Home";
 import { Login } from "./Pages/Login/Login";
 import { NotFound } from "./Pages/NotFound/NotFound";
+import { Maintenance } from "./Pages/Maintenance/Maintenance";
 
 export function App() {
 
@@ -12,6 +13,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/manutencao" element={<Maintenance />}></Route>
 
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>
