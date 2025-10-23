f58c69996459905361b61755bbb6b46727a14fdd
Vinícius Avarelo <avarelovinicius@gmail.com>
2025-10-23 01:16:07 -0300

    Add Carrinho route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from "./Pages/Home/Home";
 import { Login } from "./Pages/Login/Login";
 import { NotFound } from "./Pages/NotFound/NotFound";
 import { Feminino }from './Pages/Feminino/Feminino';
+import { Carrinho } from "./Pages/Carrinho/Carrinho";
 import Product from './Pages/Product/Product';
 
 export function App() {
@@ -14,8 +15,9 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
-  <Route path="/feminino" element={<Feminino />}></Route>
-  <Route path="/produto/:id" element={<Product />}></Route>
+        <Route path="/feminino" element={<Feminino />}></Route>
+        <Route path="/produto/:id" element={<Product />}></Route>
+        <Route path="/carrinho" element={<Carrinho />}></Route>
 
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>
