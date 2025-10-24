0aee7b8030cec168e16d7c9b42070ceca68cfb04
limmuz <miguellima.nascimento.005@gmail.com>
2025-10-24 15:24:18 -0300

    Responsividade HOME

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,33 +3,57 @@ import { Input } from "../Input/Input"
 import { Logo } from "../Logo/Logo"
 import { Nav } from "../Nav/Nav"
 import User from "../User/User"
+import { useState, useEffect } from "react"
 
 import styles from "./Header.module.css"
 
 export function Header() {
-    return (
-        <header className={`px-100 row ${styles.header} ${styles.cabecalho}`}>
-            <Logo />
+    const [isMobile, setIsMobile] = useState(false)
 
+    useEffect(() => {
+        const checkScreenSize = () => {
+            const mobile = window.innerWidth <= 768
+            setIsMobile(mobile)
+            console.log('Largura:', window.innerWidth, 'isMobile:', mobile) // DEBUG
+        }
+
+        checkScreenSize()
+        window.addEventListener('resize', checkScreenSize)
+
+        return () => window.removeEventListener('resize', checkScreenSize)
+    }, [])
+
+
+
+    return (
+        <header className={`px-100 row ${styles.cabecalho}`}>
+            <Logo />
+            
             <Input />
 
-            <ul className={`row`}>
-                <li>
-                    <NavLink to="/login">
-                        <User />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/carrinho">
-                        <Nav icon="bi-cart" />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/rastreio">
-                        <Nav icon="bi-truck" />
-                    </NavLink>
-                </li>
-            </ul>
+            {!isMobile ? (
+                <ul className={`row ${styles.navIcons}`}>
+                    <li>
+                        <NavLink to="/login">
+                            <User />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/carrinho">
+                            <Nav icon="bi-cart" />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/rastreio">
+                            <Nav icon="bi-truck" />
+                        </NavLink>
+                    </li>
+                </ul>
+            ) : (
+                <div className={styles.hamburger}>
+                    <Nav icon="bi-list" />
+                </div>
+            )}
         </header>
     )
 }
