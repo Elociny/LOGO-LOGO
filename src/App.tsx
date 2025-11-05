af3481d43a78078d04be6899b6e60370fc053793
limmuz <miguellima.nascimento.005@gmail.com>
2025-11-05 10:19:00 -0300

    Caminhos atualizados (register, forgot and change password)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import { NotFound } from "./Pages/NotFound/NotFound";
 import { Feminino }from './Pages/Feminino/Feminino';
 import { Carrinho } from "./Pages/Carrinho/Carrinho";
 import Product from './Pages/Product/Product';
+import { Register } from "./Pages/Register/Register";
+import { ForgotPassword } from "./Pages/ForgotPassword/ForgotPassword";
+import { ChangePassword } from "./Pages/ChangePassword/ChangePassword";
+import { Maintenance } from "./Pages/Maintenance/Maintenance";
 
 export function App() {
 
@@ -18,7 +22,10 @@ export function App() {
         <Route path="/feminino" element={<Feminino />}></Route>
         <Route path="/produto/:id" element={<Product />}></Route>
         <Route path="/carrinho" element={<Carrinho />}></Route>
-
+        <Route path= "/register" element={<Register />}></Route>
+        <Route path= "/forgotPassword" element={<ForgotPassword />}></Route>
+        <Route path= "/changePassword" element={<ChangePassword />}></Route>
+                
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
