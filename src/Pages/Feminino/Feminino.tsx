534bfeb459d05648814e5b615b0602f62325211e
Nicole Lins Coelho <nicolelinscoelho@gmail.com>
2025-11-18 05:15:54 -0300

    Integração da página de listagem com o back-end

--- src/Pages/Feminino/Feminino.tsx
+++ src/Pages/Feminino/Feminino.tsx
@@ -2,15 +2,19 @@ import { Product } from "../../components/Product/Product";
 import { Filters } from "../../components/Filters/Filters";
 import style from "./Feminino.module.css";
 
-import Product1 from "../../assets/images/products/produto1.svg";
-import Product2 from "../../assets/images/products/produto2.svg";
-import Product3 from "../../assets/images/products/produto3.svg";
-import Product4 from "../../assets/images/products/produto4.svg";
-import Product5 from "../../assets/images/products/produto5.svg";
 import { Layout } from "../../components/Layout/Layout";
 import { Categories } from "../../components/Categories/Categories";
+import { useQuery } from "@tanstack/react-query";
+import { listarProdutos } from "../../services/produtoService";
+import { Error } from "../../components/Error/Error";
+import { Spinner } from "../../components/Spinner/Spinner";
 
 export function Feminino() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["produtos"],
+    queryFn: listarProdutos,
+  })
+
   return (
     <Layout theme="light">
       <div className={style.container}>
@@ -22,53 +26,19 @@ export function Feminino() {
           </aside>
 
           <section className={style.products}>
+
+            {isLoading && (
+              <Spinner />
+            )}
+
+            {isError && (
+              <Error />
+            )}
+
             <div className={`${style.grid}`}>
-              <Product
-                image={Product1}
-                name="Vertical Striped Shirt"
-                price={240}
-                discount={20}
-              />
-              <Product
-                image={Product5}
-                name="Camisa Verde Listrada"
-                price={212}
-                discount={20}
-              />
-              <Product image={Product2} name="Camiseta Laranja" price={154} />
-              <Product
-                image={Product3}
-                name="Short Jeans Azul"
-                price={75}
-                discount={50}
-              />
-              <Product
-                image={Product4}
-                name="Calça Jeans Preta"
-                price={125.69}
-              />
-              <Product
-                image={Product5}
-                name="Camisa Verde Listrada"
-                price={212}
-                discount={20}
-              />
-              <Product
-                image={Product2}
-                name="Camiseta Laranja Courage"
-                price={154}
-              />
-              <Product
-                image={Product3}
-                name="Short Jeans Azul"
-                price={75}
-                discount={50}
-              />
-              <Product
-                image={Product4}
-                name="Calça Jeans Preta"
-                price={125.69}
-              />
+              {!isLoading && data?.slice(0, 4).map(produto => (
+                <Product key={produto.id} image={produto.imageUrl} name={produto.nome} price={produto.preco} discount={produto.desconto} />
+              ))}
             </div>
           </section>
         </main>
