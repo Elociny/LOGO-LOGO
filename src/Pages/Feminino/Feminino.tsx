79f8662d49cd16f3d1afd533c787aa1cbaac96ef
Nicole Lins Coelho <nicolelinscoelho@gmail.com>
2025-11-18 05:12:09 -0300

    Modificando estilos da página de listagem de produtos

--- src/Pages/Feminino/Feminino.tsx
+++ src/Pages/Feminino/Feminino.tsx
@@ -1,14 +1,6 @@
 import { Product } from "../../components/Product/Product";
-import { Category } from "../../components/Category/Category";
 import { Filters } from "../../components/Filters/Filters";
 import style from "./Feminino.module.css";
-
-import Lightning from "../../assets/images/icons/lightning.svg";
-import Dress from "../../assets/images/icons/dress.svg";
-import Shirt from "../../assets/images/icons/shirt.svg";
-import Bear from "../../assets/images/icons/bear.svg";
-import Shoe from "../../assets/images/icons/shoe.svg";
-import Necklace from "../../assets/images/icons/necklace.svg";
 
 import Product1 from "../../assets/images/products/produto1.svg";
 import Product2 from "../../assets/images/products/produto2.svg";
@@ -16,19 +8,13 @@ import Product3 from "../../assets/images/products/produto3.svg";
 import Product4 from "../../assets/images/products/produto4.svg";
 import Product5 from "../../assets/images/products/produto5.svg";
 import { Layout } from "../../components/Layout/Layout";
+import { Categories } from "../../components/Categories/Categories";
 
 export function Feminino() {
   return (
     <Layout theme="light">
       <div className={style.container}>
-        <section className={style.categories}>
-          <Category icon={Lightning} titulo="Novidades" />
-          <Category icon={Dress} titulo="Feminino" />
-          <Category icon={Shirt} titulo="Masculino" />
-          <Category icon={Bear} titulo="Infantil" />
-          <Category icon={Shoe} titulo="Calçados" />
-          <Category icon={Necklace} titulo="Acessórios" />
-        </section>
+        <Categories />
 
         <main className={style.main}>
           <aside className={style.filters}>
